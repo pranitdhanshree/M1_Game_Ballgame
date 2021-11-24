1dab0ed0435fd556d2aaa1f74fadebadc782e65c
pranitdhanshree <94231272+pranitdhanshree@users.noreply.github.com>
2021-11-24 16:41:31 +0530

    Update Ball_Operation.h

--- 3_Implementation/Src/Ball_Operation.h
+++ 3_Implementation/Src/Ball_Operation.h
@@ -8,3 +8,7 @@
  * @copyright Copyright (c) 2021
  * 
  */
+int x1,y1,x2,y2,x1old,x2old,ycenter,xcenter;
+int xnewcenter,ynewcenter,radius,ballspeedx,ballspeedy;
+char sym,pit[20];
+int score=0,speed=10;
